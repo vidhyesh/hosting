8a5c8a18bc2ab225ae8d99d4193d6486da73c368
vidhyesh <vidhyesh005@gmail.com>
2019-08-10 21:40:48 +0530

    cv

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
+// ^ middleware to redirect all URLs to index.html
+
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
+
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname ,'/dist'))
+})
+
+app.listen(80, function () {
+  console.log( 'Express serving on 80' )
+})
